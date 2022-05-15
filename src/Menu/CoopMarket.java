2787215b56e0c308c2b09fba80e07db895d11a45
Júlia Martins <juliamfbrasileiro21@gmail.com>
2022-05-15 17:16:37 -0300

    first commit

---
+++ src/Menu/CoopMarket.java
@@ -0,0 +1,12 @@
+package Menu;
+
+import Cadastro.Login.TelaLogin;
+
+public class CoopMarket {
+
+    public static void main(String[] args) {
+       TelaLogin tela = new TelaLogin();
+       tela.setVisible(true);
+    }
+    
+}
